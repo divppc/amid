69784b079cd8b821b17bd1fcf8d3d0e11c09d6a4
divppc <okal.ihor@gmail.com>
2019-05-09 18:32:26 +0300

    time/weaher done

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,21 @@
-$(".btn a").on("click", e => {
-  e.preventDefault();
+const getWeather = async () => {
+  const w = fetch(
+    "http://api.openweathermap.org/data/2.5/find?q=Beirut&units=metric&appid=9da32aaafbf99f43f7d52b9381cd91c5"
+  ).then(r => r.json());
+  const { list } = await w;
+  const currentWeather = Math.ceil(list[0].main.temp);
+  const icon = list[0].weather[0].icon;
 
-  $("html, body").animate(
-    {
-      scrollTop: $("section.cards").offset().top + 30
-    },
-    500,
-    "linear"
-  );
-});
+  currentWeather && icon && setWeather(currentWeather, icon);
+};
+
+const setWeather = (temp, icon) => {
+  const tempHolder = document.querySelector(".temp-holder");
+  const iconHolder = document.querySelector(".icon-holder");
+  tempHolder.innerHTML = `${temp} ° C`;
+  iconHolder.innerHTML = `<img src="http://openweathermap.org/img/w/${icon}.png" />`;
+
+  document.querySelector(".time").childNodes[2].remove();
+};
+
+getWeather();
